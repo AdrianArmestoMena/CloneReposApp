8c2d0b7976ac66ee4acef8b55a36aba34dcea52c
Adrian Armesto <armestomena@gmail.com>
2022-11-23 10:17:23 +0100

    Add data file

---
+++ js/data.js
@@ -0,0 +1,36 @@
+const students202209 = [
+  { githubUser: "catatarelli", name: "Catalina" },
+  { githubUser: "ZhenzhiJ", name: "Zenzhi" },
+  { githubUser: "Jacky16", name: "Adrià" },
+  { githubUser: "Jose-Ramos-Orona", name: "José" },
+  { githubUser: "mireiarull", name: "Mireia" },
+  { githubUser: "nuriarai", name: "Nuria" },
+  { githubUser: "dreapediez", name: "Andrea" },
+  { githubUser: "benjaminrae", name: "Ben_Rae" },
+  { githubUser: "pastordesoles", name: "Xavier" },
+  { githubUser: "inodna", name: "Andoni" },
+  { githubUser: "ArnauOriol", name: "Arnau" },
+  { githubUser: "vspevc", name: "Enric" },
+  { githubUser: "Champurias", name: "David" },
+  { githubUser: "Victorreca", name: "Victor" },
+  { githubUser: "jfontdev", name: "Jordi" },
+  { githubUser: "carlosdelblanco", name: "Carlos" },
+  { githubUser: "Rotciv86", name: "Victor_Merino" },
+  { githubUser: "varitoapg", name: "Alvaro" },
+  { githubUser: "oriolraventosc", name: "Oriol" },
+  { githubUser: "bertoforteza", name: "Berto" },
+  { githubUser: "CristinaFores", name: "Cristina" },
+  { githubUser: "Mibarfu", name: "Miguel" },
+  { githubUser: "albertvallsbe", name: "Albert" },
+  { githubUser: "Tutitoos", name: "Guillem" },
+  { githubUser: "rodo08", name: "Rodrigo" },
+];
+
+const path = "/Users/adrian/Desktop/clones-isdi/";
+const promotion = "202209";
+
+module.exports = {
+  students202209,
+  path,
+  promotion,
+};
